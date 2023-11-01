14c8720af51c214605f96c9ae3f87721675e078b
Alto <125269058+AltoKlef@users.noreply.github.com>
2023-11-01 16:32:58 +0300

    lab2

--- lab2/lab2/Tim.h
+++ lab2/lab2/Tim.h
@@ -2,9 +2,21 @@
 #include "DinArr.h"
 #include <algorithm>
 
-
-int GetMinrun(int n) {
-    // равно 1, если среди сдвинутых битов есть хотя бы один ненулевой
+template<typename T>
+void output(DinArr<T> arr) {//вывод элементов
+	for (int i = 0; i < arr.size(); i++) {
+		//cout <<"arr["<<i<<"]= "<< arr[i] << " ";
+        cout << arr[i] << " ";
+	}
+}
+template<typename T>
+void output(DinArr<T> arr,int first,int last) {//вывод в заданном диапазоне
+	for (int i = first; i < last; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << "\n\n\n";
+}
+int GetMinrun(int n) {//получение minrun
     int r = 0;
     while (n >= 64) {
         r |= (n & 1);
@@ -13,36 +25,113 @@ int GetMinrun(int n) {
     return n + r;
 }
 template <typename T>
-void insertion(DinArr<T>& arr, int size) {
-    T value;
-    int i;
-    for (int j = 1; j < size; j++) {
-        value = arr[j];
-        i = j - 1;
-        while (i >= 0 && arr[i] > value) {
-            arr[i + 1] = arr[i];
-            arr[i] = value;
-            i = i - 1;
-        }
+void InsertionSort(DinArr<T>&arr, int left, int right)//классическая сортировка вставками 
+{
+	for (int i = left + 1; i <= right; i++) {
+		int temp = arr[i];
+		int j = i - 1;
+		while (j >= left && arr[j] > temp) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
+	}
+}
 
+template <typename T>
+void merge(DinArr<T>& arr, int left, int mid, int right) {//модернизированное слияние 
+    int len1 = mid - left + 1;
+    DinArr<T> leftArr(len1);//создается массив для левой части
+
+    for (int i = 0; i < len1; i++) { //массив заполняется от left до mid
+        leftArr[i] = arr[left + i];
+    }
+    int i = 0, j = mid+1;
+    int k = left;
+    int leftcount=0;//счетчики для подсчета количества элементов подряд взятых из какого-то массива
+    int rightcount=0;
+    while (i < len1 && j <= right) {
+        if (leftArr[i] <=arr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+            leftcount++;
+            rightcount = 0;
+        }
+        else {
+            arr[k] = arr[j];
+            j++;
+            rightcount++;
+            leftcount = 0;
+        }
+        k++;
+        if (leftcount >= 7) { //так называемый галоп если из левого массива берут много элементов подряд
+            leftcount = 0;
+            int stepen = 0;
+            for (int ii = i + pow(2, stepen); ii< len1;ii=i+pow(2,stepen)) {
+                if (leftArr[ii] < arr[j]) {
+                    stepen++;
+                }
+                else {
+                    stepen--;
+                    ii = i + pow(2, stepen);
+                    while (i < ii) {
+                        arr[k] = leftArr[i];
+                        i++;
+                        k++;
+                    }
+                    break;
+                }
+            }
+        }
+        if(rightcount >= 7) { //галоп в случае если правый массив (аналогично)
+            rightcount = 0;
+            int stepen = 0;
+            for (int jj = j + pow(2, stepen); jj< right;jj=j+pow(2,stepen)) {
+                if (arr[jj] < arr[j]) {
+                    stepen++;
+                }
+                else {
+                    stepen--;
+                    jj = j + pow(2, stepen);
+                    while (j < jj) {
+                        arr[k] = leftArr[j];
+                        j++;
+                        k++;
+                    }
+                    break;
+                }
+            }
+        }
+        
+    }
+    //прицепляем остаток 
+    while (i < len1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < right) {
+        arr[k] = arr[j];
+        j++;
+        k++;
     }
 }
-/*template <typename T>
-void timSort(DinArr<T>& arr) {
-    int n = arr.size();
-    int MinRun = getMinrun(n);
-    for (int i = 0; i < n; i += MinRun) {
-        insertionSort(arr, i, std::min((i + MinRun - 1), (n - 1)));
-    }
-
-    for (int size = MinRun; size < n; size = 2 * size) {
+template <typename T>
+void timSort(DinArr<T>& arr,int n) {
+    int minrun = GetMinrun(n);
+	//cout << "minrun is:" << minrun << endl;
+	for (int i = 0; i < n; i += minrun) {
+		InsertionSort(arr, i, min((i + minrun - 1), n-1));//разбиение на подмассивы и сортировка их
+	}
+    for (int size = minrun; size < n; size = 2 * size) {
         for (int left = 0; left < n; left += 2 * size) {
-            int mid = std::min((left + size - 1), (n - 1));
-            int right = std::min((left + 2 * size - 1), (n - 1));
+            int mid = min((left + size - 1), (n-1));
+            int right = min((left + 2 * size - 1), n-1);
 
             if (mid < right) {
-                merge(arr, left, mid, right);
+                merge(arr, left, mid, right);//слияние массивов
             }
         }
     }
-}*/
+}
--- lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2.cpp
@@ -18,19 +18,35 @@ void randomfill(DinArr<T>& arr, int size, int min, int max) {
         arr[i] = GetRandomNumber(min, max);
     }
 }
-template<typename T>
-void output(DinArr<T> arr) {
-    for (int i = 0; i < arr.size(); i++) {
-        cout << arr[i] << " ";
+void check(DinArr<int> arr) {
+    int size = arr.size();
+    cout << endl<<"Wrong numbers:"<<endl;
+    
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < arr[i - 1]) {
+            cout <<"arr[" <<i<<"] =" << " " << arr[i] << endl;
+            cout << "arr[" << i-1 << "] =" << " " << arr[i-1] << endl;
+        }
     }
 }
 int main()
 {
+    //я не стал делать интерфейс ибо особого смысла нет (не будете же вы вводить вручную 1399229 элементов?)
     srand(time(NULL));
-    DinArr<int> array(10);
-    randomfill(array, array.size(), 1, 400);
-    output(array);
-    cout << "\n";
-    insertion(array, array.size());
-    output(array);
+    DinArr<int> array(1000000);//создаем массив размером 1000000 (для тестов можно сделать меньше)
+    randomfill(array, array.size(), 1, 1000);//1 и 1000 пределы случайных чисел 
+    //output(array);
+    timSort(array,array.size());
+   // output(array); 
+   check(array);//эта функция проверяет вручную расстановку элементов массива и выводит неправильные номерат (если есть)
+   system("pause");
+   system("cls");
+   DinArr<int> arr(200); //тут более показательно на массиве 200 элементов с выводом
+   randomfill(arr, arr.size(), 1, 1000);
+   cout << "Unsorted arr:" << endl;
+   output(arr);
+   timSort(arr, arr.size());
+   cout <<endl<< "Sorted arr:" << endl;
+   output(arr); 
+   check(arr);
 }
